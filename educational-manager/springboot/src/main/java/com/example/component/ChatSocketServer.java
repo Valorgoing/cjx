a7160862bd1dcbfec33d81dec0e784f500987728
rlagofla <miss.pinky.0825@gmail.com>
2024-05-26 05:14:06 +0800

    新增历史记录数据库

--- educational-manager/springboot/src/main/java/com/example/component/ChatSocketServer.java
+++ educational-manager/springboot/src/main/java/com/example/component/ChatSocketServer.java
@@ -6,6 +6,8 @@ import com.example.mapper.ChatroomMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -13,6 +15,7 @@ import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -21,47 +24,73 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint(value = "/chatroom/{usrInfo}")
 @Component
+@DependsOn("applicationContextHolder")
 public class ChatSocketServer {
     public static final Map<String, Session> sessionMap = new ConcurrentHashMap<>();
+    public static final Map<String, List<Session>> teamSessionMap = new ConcurrentHashMap<>();
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final List<Map<String, String>> chatHistory = new ArrayList<>();
+    private static final Map<String, List<Map<String, String>>> chatHistory = new HashMap<>();
     ChatroomMapper chatroomMapper;
+
+    @PostConstruct
+    public void init() {
+        // 加载聊天记录
+        chatroomMapper = ApplicationContextHolder.getContext().getBean(ChatroomMapper.class);
+        List<String> allTeams = chatroomMapper.getTeamList();
+        for (String team : allTeams) {
+            List<Map<String, String>> history = chatroomMapper.getChatHistoryForTeam(team);
+            chatHistory.put(team, history);
+        }
+    }
+
+    @PreDestroy
+    public void destroy() {
+        chatroomMapper.truncateHistory();
+        // 保存聊天记录
+        for (Map.Entry<String, List<Map<String, String>>> entry : chatHistory.entrySet()) {
+            chatroomMapper.saveChatHistoryForTeam(entry.getKey(), entry.getValue());
+        }
+    }
 
     @OnOpen
     public void onOpen(Session session, @PathParam(value = "usrInfo") String usrInfo) throws IOException {
         chatroomMapper = ApplicationContextHolder.getContext().getBean(ChatroomMapper.class);
-
         sessionMap.put(usrInfo, session);
-        System.out.println(usrInfo + "in");
-        System.out.println(sessionMap.size());
 
-        updateAllSessionUserList();
-        updateAllSessionChatHistory();
+        String teamName = chatroomMapper.getTeamName(parseId(usrInfo));
+        if (!chatHistory.containsKey(teamName)) chatHistory.put(teamName, new ArrayList<>());
+        if (!teamSessionMap.containsKey(teamName)) teamSessionMap.put(teamName, new ArrayList<>());
+
+        teamSessionMap.get(teamName).add(session);
+
+        updateTeamUserList(teamName);
+        updateTeamChatHistory(teamName);
     }
 
     @OnClose
-    public void onClose(@PathParam(value = "usrInfo") String usrInfo) {
+    public void onClose(Session session, @PathParam(value = "usrInfo") String usrInfo) throws IOException {
         sessionMap.remove(usrInfo);
         System.out.println(usrInfo + " leave");
         System.out.println("remain" + sessionMap.size());
 
-        updateAllSessionUserList();
+        String teamName = chatroomMapper.getTeamName(parseId(usrInfo));
+        teamSessionMap.get(teamName).remove(session);
+        updateTeamUserList(teamName);
     }
 
     @OnMessage
     public void onMessage(String msg, Session session, @PathParam(value = "usrInfo") String usrInfo) throws JsonProcessingException {
+        String teamName = chatroomMapper.getTeamName(parseId(usrInfo));
+
         Map<String, String> message = objectMapper.readValue(msg, Map.class);
-        chatHistory.add(message);
-        updateAllSessionChatHistory();
+        chatHistory.get(teamName).add(message);
+        updateTeamChatHistory(teamName);
     }
 
-    public void sendUserList(String usrInfo) {
-        String[] l = usrInfo.split("-");
-        String role = l[0];
-        String id = l[1];
+    public String getUserList(String teamName) {
 
-        List<Student> students = chatroomMapper.getTeamList(Integer.parseInt(id));
-        Teacher teacher = chatroomMapper.getTeamTeacher(Integer.parseInt(id));
+        List<Student> students = chatroomMapper.getTeamMemberList(teamName);
+        Teacher teacher = chatroomMapper.getTeamTeacher(teamName);
 
         List<Map<String, Object>> response = new ArrayList<>();
         Map<String, Object> t_pairs = new HashMap<>();
@@ -70,8 +99,6 @@ public class ChatSocketServer {
         response.add(t_pairs);
 
         for (final Student stu : students) {
-            if (id.equals(stu.getId().toString()) && "STUDENT".equals(role)) continue;
-
             Map<String, Object> pairs = new HashMap<>();
             pairs.put("name", stu.getName());
             if (sessionMap.containsKey("STUDENT-" + stu.getId().toString()))
@@ -83,48 +110,34 @@ public class ChatSocketServer {
 
 
         try {
-            Session session = sessionMap.get(usrInfo);
-            String json = objectMapper.writeValueAsString(response);
-            session.getBasicRemote().sendText(json);
+            return objectMapper.writeValueAsString(response);
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
 
-    public void updateAllSessionUserList() {
-        for (final String usrInfo : sessionMap.keySet()) {
-            sendUserList(usrInfo);
+    public void updateTeamUserList(String teamName) throws IOException {
+        for (final Session session : teamSessionMap.get(teamName)) {
+            String json = getUserList(teamName);
+            session.getBasicRemote().sendText(json);
         }
     }
 
-    public void sendChatHistory(String usrInfo) {
-        String[] l = usrInfo.split("-");
-        String id = l[1];
-
-        List<Student> students = chatroomMapper.getTeamList(Integer.parseInt(id));
-        Teacher teacher = chatroomMapper.getTeamTeacher(Integer.parseInt(id));
-
-        List<Map<String, String>> response = new ArrayList<>();
-        for (final Map<String, String> aMsg : chatHistory) {
-            if (students.stream().anyMatch(stu -> stu.getName().equals(aMsg.get("sender"))) ||
-                teacher.getName().equals(aMsg.get("sender"))) {
-                response.add(aMsg);
+    public void updateTeamChatHistory(String teamName) {
+        for (final Session session : teamSessionMap.get(teamName)) {
+            try {
+                String json = objectMapper.writeValueAsString(chatHistory.get(teamName));
+                session.getBasicRemote().sendText(json);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-
-        try {
-            Session session = sessionMap.get(usrInfo);
-            String json = objectMapper.writeValueAsString(response);
-            session.getBasicRemote().sendText(json);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
-    public void updateAllSessionChatHistory() {
-        for (final String usrInfo : sessionMap.keySet()) {
-            sendChatHistory(usrInfo);
-        }
+    private Integer parseId(String usrInfo) {
+        String[] l = usrInfo.split("-");
+        return Integer.parseInt(l[1]);
     }
 
 }
--- educational-manager/springboot/src/main/java/com/example/mapper/ChatroomMapper.java
+++ educational-manager/springboot/src/main/java/com/example/mapper/ChatroomMapper.java
@@ -1,14 +1,26 @@
 package com.example.mapper;
 
-import com.example.entity.Attendance;
 import com.example.entity.*;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
+@Mapper
 public interface ChatroomMapper {
-    List<Student> getTeamList(Integer stuId);
+    List<Student> getTeamMemberList(String teamName);
 
-    Teacher getTeamTeacher(Integer stuId);
+    Teacher getTeamTeacher(String teamName);
+
+    String getTeamName(Integer stuId);
+
+    List<String> getTeamList();
+
+    @MapKey("sender")
+    List<Map<String, String>> getChatHistoryForTeam(String teamName);
+
+    void truncateHistory();
+    void saveChatHistoryForTeam(String teamName, @Param("historyList") List<Map<String, String>> historyList);
 }
